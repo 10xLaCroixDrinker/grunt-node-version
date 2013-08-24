38dcd79247047d095946f0d4b1599ff7f1879647
Jimmy King <jimmy.king@meltmedia.com>
2013-08-23 18:37:25 -0700

    Import initial task work

--- tasks/node_version.js
+++ tasks/node_version.js
@@ -16,35 +16,19 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('node_version', 'A grunt task to ensure you are using the Node version required by your project's package.json', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      errorLevel: 'fatal',
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    var expected = grunt.file.readJSON('package.json').engines.node,
+        actual = process.version,
+        result = semver.satisfies(actual, expected);
+    
+    if (result === true) {
+      return;
+    } else {
+      grunt.fail.fatal('Expected Node v' + expected + ', but found ' + actual);
+    }
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
   });
 
 };
